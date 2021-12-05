c6bc176c969904733d3627d1f0c05cd183c31aa0
pullpushtree <bad7legend@gmail.com>
2021-12-05 17:02:17 -0500

    Added property card and list components

--- src/app/property/property-card/property-card.component.spec.ts
+++ src/app/property/property-card/property-card.component.spec.ts
@@ -1,10 +1,13 @@
+import { DebugElement, Query } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PropertyCardComponent } from './property-card.component';
 
 describe('PropertyCardComponent', () => {
   let component: PropertyCardComponent;
   let fixture: ComponentFixture<PropertyCardComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,4 +25,16 @@ describe('PropertyCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show homes', () => {
+    expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(1);
+  });
+
+  it('should show home info', () => {
+     const home = fixture.nativeElement.querySelector('[data-test="home"]');
+    expect(home.querySelector('[data-test="image"]')).toBeTruthy();
+    expect(home.querySelector('[data-test="name"]')).toBeTruthy();
+    expect(home.querySelector('[data-test="type"]')).toBeTruthy();
+    expect(home.querySelector('[data-test="price"]')).toBeTruthy();
+  })
 });
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+  {
+    "Id": 1,
+    "Name":"Rich Man",
+    "Type":"House",
+    "Price":18000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 2,
+    "Name":"Jan Man",
+    "Type":18,
+    "Price":10000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 3,
+    "Name":"Yan Man",
+    "Type":"House",
+    "Price":2000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 4,
+    "Name":"Fan Man",
+    "Type":"House",
+    "Price":6000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 5,
+    "Name":"Brenn Man",
+    "Type":"House",
+    "Price":8000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 6,
+    "Name":"Stan Man",
+    "Type":"House",
+    "Price":42000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 7,
+    "Name":"Glen Man",
+    "Type":"House",
+    "Price":20000,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 8,
+    "Name":"Stef Man",
+    "Type":"House",
+    "Price":8500,
+    "Image":"assets/images/house.png"
+  },
+  {
+    "Id": 9,
+    "Name":"Jeff Man",
+    "Type":"House",
+    "Price":67000,
+    "Image":"assets/images/house.png"
+  },{
+    "Id": 10,
+    "Name":"Kat Man",
+    "Type":"House",
+    "Price":54200,
+    "Image":"assets/images/house.png"
+  },
+];
+
   constructor() { }
 
   ngOnInit(): void {
